1ba8263f157273f5f6bff5a9081861185cf12402
Oleg Shtein <olegshteyn@gmail.com>
2025-09-26 14:32:23 +0700

    build: add and configure vite-plugin-svgr

--- src/shared/types/global.d.ts
+++ src/shared/types/global.d.ts
@@ -2,3 +2,22 @@ declare module '@a1rth/css-normalize' {
   const scss: string
   export default scss
 }
+
+declare module '*.svg' {
+  const src: string
+  export default src
+
+  import * as React from 'react'
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >
+}
+
+declare module '*.svg?react' {
+  import * as React from 'react'
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >
+  export default ReactComponent
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        dimensions: false,
+        replaceAttrValues: {
+          '#000': 'currentColor',
+          '#000000': 'currentColor',
+          'black': 'currentColor',
+        },
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+              active: false,
+            },
+          ],
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
